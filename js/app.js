c05f0dba82762562e8c36fbd101e75fcbd13a730
zobayertihan <zobayertihan@gmail.com>
2022-09-02 21:53:17 +0600

    data-view

--- js/app.js
+++ js/app.js
@@ -6,19 +6,48 @@ const loadMenuData = async () => {
 
 setFullMenu = async () => {
     const data = await loadMenuData();
-    console.log(data.data.news_category);
     const menu = document.getElementById('allMenu');
     const menuArray = [];
 
     for (const section of data.data.news_category) {
-        console.log(section.category_name);
         if (menuArray.indexOf(section.category_name) === -1) {
             menuArray.push(section.category_name);
             const li = document.createElement('li');
-            li.innerHTML = `<a>${section.category_name}</a>`;
+            li.innerHTML = `<a id="${section.category_id}">${section.category_name}</a>`;
             menu.appendChild(li);
         }
     }
 }
 
+loadNewsData = async () => {
+    const response = await fetch('https://openapi.programming-hero.com/api/news/category/01');
+    const data = await response.json();
+    return data;
+}
+showNewsData = async () => {
+    const datas = await loadNewsData();
+    for (const data of datas.data) {
+        console.log(data)
+        const viewData = document.getElementById('showNews');
+        const createDiv = document.createElement('div');
+        createDiv.innerHTML = `
+            <div class="card lg:card-side bg-base-100 shadow-xl">
+                <figure><img class="" src="${data.image_url}" alt=""></figure>
+                <div class="card-body">
+                    <h2 class="card-title">New album is released!</h2>
+                    <p>Click the button to listen on Spotiwhy app.</p>
+                    <div class="card-actions justify-end">
+                        <button class="btn btn-primary">Listen</button>
+                    </div>
+                </div>
+            </div>`
+        viewData.appendChild(createDiv)
+    }
+
+
+
+
+
+}
+showNewsData()
 setFullMenu()
