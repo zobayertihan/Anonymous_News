7245f971309bac48e5489e4888f72f0a08a3cfb0
zobayertihan <zobayertihan@gmail.com>
2022-09-03 04:01:46 +0600

    Calculating

--- js/app.js
+++ js/app.js
@@ -6,6 +6,7 @@ const loadMenuData = async () => {
 
 setFullMenu = async () => {
     const data = await loadMenuData();
+    // console.log(data)
     const menu = document.getElementById('allMenu');
     const menuArray = [];
 
@@ -13,47 +14,65 @@ setFullMenu = async () => {
         if (menuArray.indexOf(section.category_name) === -1) {
             menuArray.push(section.category_name);
             const li = document.createElement('li');
-            li.innerHTML = `<a id="${section.category_id}">${section.category_name}</a>`;
+            li.innerHTML = `<button onclick="loadNewsData('${section.category_id}');toggleSpinner(${true})" id="">${section.category_name}</button>`;
             menu.appendChild(li);
         }
     }
 }
 
-loadNewsData = async () => {
-    const response = await fetch('https://openapi.programming-hero.com/api/news/category/04');
-    const data = await response.json();
-    return data;
+const loadNewsData = id => {
+    const url = `https://openapi.programming-hero.com/api/news/category/${id}`
+    // console.log(url);
+    // `https://openapi.programming-hero.com/api/news/category/${id}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => showNewsData(data.data))
 }
-showNewsData = async () => {
-    const datas = await loadNewsData();
-    for (const data of datas.data) {
-        console.log(data)
-        const viewData = document.getElementById('showNews');
+// Default Homepage
+loadNewsData("04")
+
+const showNewsData = newses => {
+    // console.log(newses[0])
+    const viewData = document.getElementById('showNews');
+    viewData.innerHTML = ``;
+    const totalNews = newses.length;
+    console.log(totalNews);
+    const noNewsFound = document.getElementById('no-news-found');
+    if (totalNews == 0) {
+        noNewsFound.classList.remove('hidden');
+    }
+    else {
+        noNewsFound.classList.add('hidden');
+    }
+    for (const news of newses) {
+        // console.log(news._id)
         const createDiv = document.createElement('div');
         createDiv.innerHTML = `
             <div class="card lg:card-side bg-base-100 shadow-xl my-10">
-                <figure><img class="object-contain" src="${data.thumbnail_url}" alt=""></figure>
+                <figure><img class="object-contain" src="${news.thumbnail_url}" alt=""></figure>
                 <div class="card-body">
-                    <h2 class="card-title">${data.title}</h2>
-                    <p>${data.details.slice(0, 500)}...</p>
+                    <h2 class="card-title">${news.title}</h2>
+                    <p>${news.details.slice(0, 500)}...</p>
                     <div class="card-actions justify-between">
                         <div class="flex">
-                            <figure><img class="object-contain w-12 rounded-full" src="${data.author.img}" alt=""></figure>
+                            <figure><img class="object-contain w-12 rounded-full" src="${news.author.img}" alt=""></figure>
                             <div class="ml-2">
-                                <p>${data.author.name}</p>
-                                <p>${data.author.published_date}</p>
+                                <p>${news.author.name === null ? "No data Available" : news.author.name}</p>
+                                <p>${news.author.published_date === null ? "No data Available" : news.author.published_date}</p>
                             </div>
                         </div>
                         <div class="ml-2">
-                                <p>Views: ${data.total_view}</p>
+                                <p>Views: ${news.total_view > 0 ? news.total_view : "No Data Available"}</p>
                             </div>
-                        
-                        <label onclick="viewnewsDetails('${data._id}')" for="my-modal-3" class="btn modal-button">Details</label>
+
+                        <label onclick="viewnewsDetails('${news._id}')" for="my-modal-3" class="btn modal-button">Details</label>
+                    
                     </div>
                 </div>
             </div>`
         viewData.appendChild(createDiv)
     }
+    toggleSpinner(false);
 }
 
 viewnewsDetails = async (id) => {
@@ -70,14 +89,29 @@ viewnewsDetails = async (id) => {
     const modalBody = document.getElementById('modal-body');
     modalBody.innerHTML = ``;
     modalBody.innerHTML = `
-    <h2>Author: ${author.name}</h2>
+    <h2>Author: ${author.name === "system" ? "No data Available" : author.name}</h2>
     <img src="${author.img}">
     <p>Published Date: ${author.published_date}</p>
     <img src="${thumb}">
     <p class="py-4">${details}</p>
     `
-    console.log(datas);
+}
+const toggleSpinner = isLoading => {
+    const loaderSection = document.getElementById('loader');
+    if (isLoading) {
+        loaderSection.classList.remove('hidden');
+    }
+    else {
+        loaderSection.classList.add('hidden')
+    }
+}
+
+const countNews = () => {
+    const showContainer = document.getElementById('countNews');
 
 }
-showNewsData()
+
+
+
+// showNewsData()
 setFullMenu()
